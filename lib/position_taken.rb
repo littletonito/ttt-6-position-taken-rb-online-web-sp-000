78e75097252486fcf1712005fd1c872253df85f5
Antonio Clavijo Cortes <littletonito@gmail.com>
2021-03-07 00:39:45 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -8,7 +8,10 @@ def position_taken?(board, index)
     false
   elsif (board[index]) == ""
     false
-  
+  elsif board[index] == nil
+    false
+  elsif board[index] == "X" || "O"
+    true
   end
 
 end
