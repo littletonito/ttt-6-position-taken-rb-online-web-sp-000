0e8950359b433759771a3d515aeb8253d1ba8c59
Antonio Clavijo Cortes <littletonito@gmail.com>
2021-03-07 00:40:22 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -4,12 +4,12 @@ board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 index = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
 def position_taken?(board, index)
-  if (board[index]) == " "
+  if (board[index]) == " " || "" || nil
     false
-  elsif (board[index]) == ""
-    false
-  elsif board[index] == nil
-    false
+  # elsif (board[index]) == ""
+  #   false
+  # elsif board[index] == nil
+  #   false
   elsif board[index] == "X" || "O"
     true
   end
